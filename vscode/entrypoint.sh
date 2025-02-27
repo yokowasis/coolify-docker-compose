16139542439749842bf38ad70df7e0a892b86e47
Your Name <you@example.com>
2025-02-27 04:19:09 +0000

    vscode

--- vscode/entrypoint.sh
+++ vscode/entrypoint.sh
@@ -5,8 +5,10 @@ echo "Starting Apache..."
 service apache2 start
 
 echo "Starting MariaDB..."
-service mysql start
+service mariadb start
 
 echo "Starting VS Code server..."
-. /opt/nvm/nvm.sh
-exec /usr/bin/code serve-web --host 0.0.0.0 --port 8080 --connection-token ${TOKEN}
+/opt/nvm/nvm.sh
+
+/usr/bin/code serve-web --host 0.0.0.0 --port 8080 --connection-token ${TOKEN}
+
